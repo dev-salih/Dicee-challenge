6a3e09e9b2bd04df2af15f362f400a41a4b44d8b
dev-salih <salihmu79@gmail.com>
2026-04-07 15:02:38 +0300

    some comments added

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,6 +1,7 @@
-var randomNumber1 = Math.floor(Math.random() * 6 + 1);
-var randomDiceImage1 = "dice" + randomNumber1 + ".png";
-var randomImageSource1 = "images/" + randomDiceImage1;
+var randomNumber1 = Math.floor(Math.random() * 6 + 1); //1-6
+var randomDiceImage1 = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource1 = "images/" + randomDiceImage1;  //images/dice1.png - images/dice6.png
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource1);
@@ -12,6 +13,8 @@ var image2 = document.querySelectorAll("img")[1];
 
 image2.setAttribute("src", randomImageSourse2);
 
+
+//If player 1 wins
 if(randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML="🚩palyer 1 win!!";
 } else if (randomNumber2 > randomNumber1) {
