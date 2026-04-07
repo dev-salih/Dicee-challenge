c345f95bc711a1ced7b21217cd99ed54210334ce
dev-salih <salihmu79@gmail.com>
2026-04-07 13:56:19 +0300

    image1 fully randomly setted

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,4 +1,6 @@
 var randomNumber1 = Math.floor(Math.random()* 6);
 var randomImage1 = "dice" + randomNumber1 + ".png";
 var randomImageResource1 = "images/" + randomImage1;
-document.querySelectorAll("img")[0].setAttribute("src", randomImageResource1)
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageResource1);
