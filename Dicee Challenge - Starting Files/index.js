420d899dfb7a8e85c71fd58740c31f91422af1c2
dev-salih <salihmu79@gmail.com>
2026-04-07 14:14:52 +0300

    image2 randomly setted

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,6 +1,16 @@
-var randomNumber1 = Math.floor(Math.random()* 6);
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
 var randomImage1 = "dice" + randomNumber1 + ".png";
 var randomImageResource1 = "images/" + randomImage1;
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageResource1);
+
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+var randomimage2 = "dice" + randomNumber2 +".png";
+var randomImageResourse2 = "images/" + randomimage2;
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageResourse2);
