b2336e3822fcad04551569480bef1ef290d01168
dev-salih <salihmu79@gmail.com>
2026-04-07 14:53:29 +0300

    player 1&2 has been compared

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,16 +1,21 @@
 var randomNumber1 = Math.floor(Math.random() * 6 + 1);
-var randomImage1 = "dice" + randomNumber1 + ".png";
-var randomImageResource1 = "images/" + randomImage1;
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomImageSource1 = "images/" + randomDiceImage1;
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImageResource1);
-
-
+image1.setAttribute("src", randomImageSource1);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6 + 1);
-var randomimage2 = "dice" + randomNumber2 +".png";
-var randomImageResourse2 = "images/" + randomimage2;
+var randomImageSourse2 = "images/dice" + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1];
 
-image2.setAttribute("src", randomImageResourse2);
+image2.setAttribute("src", randomImageSourse2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="🚩palyer 1 win!!";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML="🚩palyer 2 win!!";
+} else {
+    document.querySelector("h1").innerHTML="Draw!!";
+}
